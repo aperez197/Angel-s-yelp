438917c217bd1d260a3e0506f3aae37f592fc16d
aperez197 <112669081+aperez197@users.noreply.github.com>
2022-10-31 20:30:09 -0500

    4hr

---
+++ src/App.jsx
@@ -0,0 +1,30 @@
+import React from "react";
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Home from "./routes/Home";
+import UpdatePage from "./routes/UpdatePage";
+import RestaurantDetailPage from "./routes/RestaurantDetailPage";
+const App = () => {
+  return (
+    
+      <div className="container">
+        <Router>
+          <Routes>
+            <Route exact path="/" element={<Home/>}/>
+            <Route
+              exact
+              path="/restaurants/:id/update"
+              element={UpdatePage}
+            />
+            <Route
+              exact
+              path="/restaurants/:id"
+              component={RestaurantDetailPage}
+            />
+          </Routes>
+        </Router>
+      </div>
+   
+  );
+};
+
+export default App;
